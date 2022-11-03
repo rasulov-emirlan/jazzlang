fe2ec88c42c167b4c27f38821b44318ec66c0fbc
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-11-03 22:05:32 +0600

    🚧: start working on lexing

---
+++ src/lexer/lexer.go
@@ -0,0 +1,57 @@
+package lexer
+
+import "github.com/rasulov-emirlan/jazzlang/src/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	currentChar  byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar() // state position before starting lexing
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.currentChar {
+	case '=':
+		tok = newToken(token.ASSIGN, l.currentChar)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.currentChar)
+	case '(':
+		tok = newToken(token.LPAREN, l.currentChar)
+	case ')':
+		tok = newToken(token.RPAREN, l.currentChar)
+	case ',':
+		tok = newToken(token.COMMA, l.currentChar)
+	case '+':
+		tok = newToken(token.PLUS, l.currentChar)
+	case '{':
+		tok = newToken(token.LBRACE, l.currentChar)
+	case '}':
+		tok = newToken(token.RBRACE, l.currentChar)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.currentChar = 0
+	} else {
+		l.currentChar = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
