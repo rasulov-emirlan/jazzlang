25c40fe170f2fbad9fcde3d6b0ab659b3149091e
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-11-04 23:25:57 +0600

    📦: add basic structure for 'ast' and 'parser' packages️

---
+++ src/ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import "github.com/rasulov-emirlan/jazzlang/src/token"
+
+type (
+	Node interface {
+		TokenLiteral() string
+	}
+
+	Statement interface {
+		Node
+		statementNode()
+	}
+
+	Expression interface {
+		Node
+		expressionNode()
+	}
+)
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type VarStatement struct {
+	Token token.Token // the token.VAR token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *VarStatement) statementNode() {}
+
+func (ls *VarStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
