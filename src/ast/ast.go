6cdafc55d478ad368b00ede2e3c0aebc6b6cd431
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-11-29 23:23:41 +0600

    💥: add boolan parsers and modify tests

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -192,3 +192,18 @@ func (ie *InfixExpression) String() string {
 
 	return out
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
