c94abb08d2874c55146109a006c84a2820a01d65
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2023-01-28 13:19:13 +0600

    ✨: create 'object' package and add integer, boolean, null objects into it

---
+++ src/object/object.go
@@ -0,0 +1,44 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	OBJ_INTEGER = "INTEGER"
+	OBJ_BOOLEAN = "BOOLEAN"
+	OBJ_NULL    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+
+	// Returns a string representation of the object.
+	// Most of the time, this will be the literal value of the object.
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+var _ Object = (*Integer)(nil)
+
+func (i *Integer) Type() ObjectType { return OBJ_INTEGER }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+var _ Object = (*Boolean)(nil)
+
+func (b *Boolean) Type() ObjectType { return OBJ_BOOLEAN }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+var _ Object = (*Null)(nil)
+
+func (n *Null) Type() ObjectType { return OBJ_NULL }
+func (n *Null) Inspect() string  { return "null" }
