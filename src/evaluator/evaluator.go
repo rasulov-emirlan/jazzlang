ef8b38a1025b0f7c51f28e8750dddc663d35620b
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2023-02-24 20:20:36 +0600

    ✨: add 'len' funtion for arrays

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -21,6 +21,8 @@ var (
 				switch arg := args[0].(type) {
 				case *object.String:
 					return &object.Integer{Value: int64(len(arg.Value))}
+				case *object.Array:
+					return &object.Integer{Value: int64(len(arg.Elements))}
 				default:
 					return newError("argument to `len` not supported, got %s", args[0].Type())
 				}
